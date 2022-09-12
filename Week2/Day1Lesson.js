a4d6d2eb848426f2b946d8e3725ab1c08a288789
tyty-04 <tylerseisner@gmail.com>
2022-09-12 10:53:38 +0300

    renamed

---
+++ Week2/Day1Lesson.js
@@ -0,0 +1,19 @@
+let string = "Hello I'm a JS code";
+console.log(string);
+
+let me = {
+  name: "Tyler",
+  height: "6 Foot 1 Inch",
+  age: 18,
+  city: "Winston-Salem",
+};
+
+console.log(me.city);
+
+let str1 = "Hello, ";
+let str2 = "World!";
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log(str3.indexOf("W"));
--- Week2/Day1Practice.js
+++
@@ -1,2 +0,0 @@
-let string = "Hello I'm a JS code";
-console.log(string);
