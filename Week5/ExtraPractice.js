bad8769a336945a5bb514c6b2bf2f52e50266c7f
tyty-04 <tylerseisner@gmail.com>
2022-10-20 14:10:55 +0300

    Week 5 Update

---
+++ Week5/ExtraPractice.js
@@ -0,0 +1,64 @@
+// Exercise 1
+// function biggestNumberInArray(arrayNumber) {
+//   let biggest = arrayNumber[0];
+//   arrayNumber.forEach((element) => {
+//     if (element > biggest || isNaN(biggest)) {
+//       biggest = element;
+//     }
+//   });
+//   return isNaN(biggest) ? 0 : biggest;
+// }
+
+// const array = [-1, 0, 3, 100, 99, 2, 99];
+// const array2 = ["a", 3, 4, 2];
+// const array3 = [];
+
+// console.log(biggestNumberInArray(array));
+// console.log(biggestNumberInArray(array2));
+// console.log(biggestNumberInArray(array3));
+
+//Exercise 2
+// function starFrame() {
+//   let words = prompt("Enter words separated by commas");
+//   let wordsArray = [];
+//   while (words.indexOf(',') > 0) {
+//     let comma = words.indexOf(",");
+//     wordsArray.push(words.substring(0, comma));
+//     words = words.substring(comma + 2, words.length);
+//   }
+//   wordsArray.push(words.substring(0, words.length));
+//   let longest = wordsArray[0];
+//   wordsArray.forEach((element) => {
+//     if (element > longest) longest = element;
+//   });
+//   numStars = longest.length + 4;
+//   for (let i = 0; i < numStars; i++) {
+//     console.log("*");
+//   }
+//   for (let i = 0; i < wordsArray.length; i++) {
+//     let line = "* " + wordsArray[i];
+//     let spaces = numStars - line.length - 1;
+//     for (let i = 0; i < spaces; i++){
+//         line += " ";
+//     }
+//     line += "*";
+//     console.log(line);
+//   }
+//   for (let i = 0; i < numStars; i++) {
+//     console.log("*");
+//   }
+// }
+
+// starFrame();
+
+//Exercise 3
+// function checkYear() {
+//   let year = prompt("Enter a year");
+//   year > 2000
+//     ? console.log("You are in the 21st century")
+//     : console.log("You live in the middle age");
+// }
+
+// checkYear();
+
+//Exercise 4
